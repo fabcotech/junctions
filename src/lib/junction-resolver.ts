9ea6c61c92f7c773b3cb1503c0f487be730423b7
Paul Musso <paul@musso.io>
2022-11-21 11:03:46 +0100

    chore: lib, server, cli folders

--- src/lib/junction-resolver.ts
+++ src/lib/junction-resolver.ts
@@ -1,6 +1,8 @@
-import { NameRecord } from 'types';
+import { NameRecord } from './types';
 
 export function resolveJunction(junction: string): NameRecord[] {
+  // const pa parse(string);
+
   return [
     { name: junction, type: 'TXT', data: 'HASH=abddef' },
     { name: junction, type: 'A', data: '127.0.0.1' },
