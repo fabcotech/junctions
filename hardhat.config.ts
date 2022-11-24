6e75dd02cb083004d853c75622fc49f3131b0bc0
Paul Musso <paul@musso.io>
2022-11-24 12:40:24 +0100

    docs: add build:all command

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,10 +2,6 @@ import { HardhatUserConfig } from 'hardhat/config';
 import process from 'process';
 import '@nomicfoundation/hardhat-toolbox';
 
-if (process.env.PRIVATE_KEY === undefined) {
-  throw new Error('PRIVATE_KEY is not defined');
-}
-
 const config: HardhatUserConfig = {
   solidity: '0.8.17',
   networks: {
@@ -14,7 +10,7 @@ const config: HardhatUserConfig = {
     },
     bnbtestnet: {
       url: 'https://bsc-testnet.nodereal.io/v1/a49c22a98e96491085f76cfa15a3e901',
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: [process.env.PRIVATE_KEY || ''],
     },
   },
 };
